75ee604f89636b376a6321463d823423d0ec3434
kdublam <kdublam@gmail.com>
2019-05-22 22:16:29 -0700

    recommit with gitingore

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+	{
+		name: "Joey",
+		photo: "https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Chandler",
+		photo: "https://upload.wikimedia.org/wikipedia/en/6/6c/Matthew_Perry_as_Chandler_Bing.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Ross",
+		photo: "https://upload.wikimedia.org/wikipedia/en/6/6f/David_Schwimmer_as_Ross_Geller.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Rachel",
+		photo: "https://upload.wikimedia.org/wikipedia/en/e/ec/Jennifer_Aniston_as_Rachel_Green.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Phoebe",
+		photo: "https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+  },
+  {
+		name: "Monica",
+		photo: "https://upload.wikimedia.org/wikipedia/en/d/d0/Courteney_Cox_as_Monica_Geller.jpg",
+		scores: [3, 5, 4, 1, 5, 4, 3, 5, 4, 2]
+	}
+];
+
+module.exports = friends;
